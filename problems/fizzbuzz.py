cf2c7b23434cf0ac1d82fc6e80805b0926632624
Alex Hanson <>
2021-05-14 03:47:24 +0000

    smallest amount of chars for fizzbuzz code in python

---
+++ problems/fizzbuzz.py
@@ -0,0 +1,4 @@
+#84
+for i in range(101):print("fizz" if i % 3 == 0 else "fizzbuzz" if i % 5 == 0 else i)
+#97
+# for s in ["fizz" if i % 3 == 0 else "fizzbuzz" if i % 5 == 0 else i for i in range(101)]:print(s)
